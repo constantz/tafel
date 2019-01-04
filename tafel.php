53645edb8c85d2f29131078b69e43f31f9f15314
Constant <c.zwijnenberg@gmail.com>
2019-01-04 16:03:59 +0100

    small css update

--- tafel.php
+++ tafel.php
@@ -1,20 +1,23 @@
 <html>
     <head>
-        <!-- <link rel="stylesheet" href="css.css"> -->
+        <link rel="stylesheet" href="tafel.css">
         <title>Tafelgigant</title>
     </head>
 <body>
+<div class="container">
     <h1>Bestelformulier</h1>
     <h2>Houten tafels</h2>
     <form action="tafel.php" method="post">
-    <fieldset>
+    <fieldset id="naw">
         Naam:<br><input id="naam" name="naam" type="text" required><br><br>
         Leeftijd:<br><input id="leeftijd" name="leeftijd" type="text" required><br><br>
         Kortingscode:<br><input id="kortingsCode" name="kortingsCode" type="text">
         <input type="submit" value="OK">
     </fieldset>
-    <h2>Stel uw eigen tafel samen:</h2>
-    <fieldset>
+
+    <br><h2>Stel uw eigen tafel samen:</h2>
+
+    <fieldset id="kies">
         Houtsoort:<br>
         <select id="houtSoort" name="houtSoort">
             <option value="grenen">Grenen</option>
@@ -28,6 +31,7 @@
             <option value ="3">3 meter</option>
             <option value ="4">4 meter</option>
             <option value ="5">5 meter</option>
+            <option value ="6">6 meter</option>
         </select><br><br>
         Breedte:<br>
         <select id="breedte" name="breedte">
@@ -36,14 +40,19 @@
             <option value ="2">2 meter</option>
             <option value ="2.5">2,5 meter</option>
             <option value ="3">3 meter</option>
+            <option value ="3.5">3,5 meter</option>
+            <option value ="4">4 meter</option>
         </select><br><br>
         Hoogte:<br>
         <select id="hoogte" name="hoogte">
+        <option value ="60">60 cm</option>
             <option value ="70">70 cm</option>
             <option value ="80">80 cm</option>
             <option value ="90">90 cm</option>
             <option value ="100">100 cm</option>
             <option value ="110">110 cm</option>
+            <option value ="120">120 cm</option>
+            <option value ="130">130 cm</option>
         </select><br><br>
         Model:<br>
         <select id="model" name="model">
@@ -57,12 +66,13 @@
         <input type="submit" value="OK">
     </fieldset>
     </form>
+   
     
     <?php
         if ($_SERVER["REQUEST_METHOD"] == "POST"){
             $name = $_POST["naam"];
             $age = $_POST["leeftijd"];
-            echo ($age > 18) ? "<b>Factuur</b><br><br>Te leveren aan: $name <br><br>" :  "Wij kunnen helaas alleen leveren aan meerderjarigen.<br>";
+            echo ($age > 18) ? "<b>Offerte</b><br><br>Te leveren aan: $name <br><br>" :  "Wij kunnen helaas alleen leveren aan meerderjarigen.<br>";
             // $houtSoort = ucfirst($_POST["houtSoort"]);
             // echo $houtSoort;
 
@@ -86,7 +96,6 @@
             $couponFactor = 1; // voor als er kortingscodes worden gebruikt
             $modelOmschrijving = "";
             
-
             if ($aantal >= 25){
                 $aantalFactor = 0.85;
                 $aantalGrens = 25;
@@ -105,8 +114,7 @@
                     ($kortingsCode == 312) || ($kortingsCode == 321))
                         {$couponFactor = 0.9;}
                 elseif (($kortingsCode == 1234) || ($kortingsCode == 4321))
-                        {$couponFactor = 0.8;}
-                
+                        {$couponFactor = 0.8;} 
             }
 
             switch($model) {
@@ -114,8 +122,8 @@
                 case 'klassiekzwaar': $modelOmschrijving = "Klassiek, zwaar"; break;
                 case 'modernlicht': $modelOmschrijving = "Modern, licht"; break;
                 case 'modern': $modelOmschrijving = "Modern"; break;
-
             }
+
             switch($hout) {                             
                 case 'grenen': $houtFactor = 1; break;
                 case 'eiken': $houtFactor = 2; break;
@@ -137,23 +145,23 @@
                 echo "Houtsoort: " . ucfirst($_POST["houtSoort"]) . "<br>";
                 echo "Bladoppervlak: " . $opp . " m<sup>2</sup><br>";
                 echo "Hoogte: " . $hoogte . " cm<br>";
-                echo "Basisprijs: " . $basisPrijs . "<br><br>";
+                echo "Basisprijs: &euro;" . $basisPrijs . "<br><br>";
 
                 if ($aantal >= 3){
                     echo "Korting op basisprijs bij aanschaf van " . $aantalGrens . " of meer tafels: " . (100 * (1 - $aantalFactor)) . "%<br>";
-                    echo "Nieuwe basisprijs: " . $basisPrijs * $aantalFactor . "<br><br>";
+                    echo "Nieuwe basisprijs: &euro;" . $basisPrijs * $aantalFactor . "<br><br>";
                 }
 
                 if ($couponFactor != 1){
                     echo "Kortingscode \"" . $kortingsCode . "\" ingevoerd voor een extra korting van " . (100 * (1 - $couponFactor)) ."%
                     op het eindbedrag <br><br>";
-                    echo "Te betalen: " . 100*$couponFactor . "% x " . $aantal . " x " . $basisPrijs * $aantalFactor . " =  <b>" . $teBetalen * $couponFactor . "</b></br>";
+                    echo "Te betalen: " . 100*$couponFactor . "% x " . $aantal . " x " . $basisPrijs * $aantalFactor . " =  <b>&euro;" . $teBetalen * $couponFactor . "</b></br>";
                 }
-                else echo "Te betalen: " . $aantal . " x " . $basisPrijs * $aantalFactor . " =  <b>" . $teBetalen . "</b></br>";
+                else echo "Te betalen: " . $aantal . " x " . $basisPrijs * $aantalFactor . " =  <b>&euro;" . $teBetalen . "</b></br>";
             }    
-
         }    
     ?>
+</div>     
 </body>
 
 </html>
